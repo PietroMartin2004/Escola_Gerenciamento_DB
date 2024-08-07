7b3fb8a5a617b99e22182d1df7a65a61d2ae97c4
Pietro Lima Martin <kyamasuki@gmail.com>
2024-08-07 16:29:52 -0300

    sim
    teste do commit

--- menu.py
+++ menu.py
@@ -8,7 +8,7 @@ def menu():
           \n 4 - Alterar Dados Cadastrais:
           \n 5 - Excluir Aluno:
           \n 6 - Sair
-          \n 7- Tapioca xD""")
+          \n 7- Olá""")
     print("############################################")
     #Declaração, valor e retorno da função opcao para inclui-lá no arquivo main.
     global opcao
